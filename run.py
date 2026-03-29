f4546a91e8bcdc4460d48093e07e5ef2caf3df59
localrice <borokinjalkavish@gmail.com>
2026-03-29 12:08:05 +0530

    update: routes

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from app.routes.index import index_route
 from app.routes.enroll import enroll_route
 from app.services.db_service import init_db
+from app.routes.attendance import attendance_route
 
 init_db()
 app = Flask(__name__, template_folder="app/templates")
 
-app.add_url_rule("/", view_func=enroll_route, methods=["GET", "POST"])
+app.add_url_rule("/", view_func=index_route, methods=["GET"])
+app.add_url_rule("/enroll", view_func=enroll_route, methods=["GET", "POST"])
+app.add_url_rule("/attendance", view_func=attendance_route, methods=["GET", "POST"])
+
 if __name__ == "__main__":
     app.run(debug=True)
